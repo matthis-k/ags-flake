7cc5f776469f3fe1d2d1f795e789e7c96e88b2f1
matthis-k <matthis.kaelble@gmail.com>
2025-02-01 20:24:10 +0100

    specify on_click and on_scroll type for StatusIcon

--- widget/StatusIcon.tsx
+++ widget/StatusIcon.tsx
@@ -1,12 +1,13 @@
 import { bind, Binding } from "astal"
 import Astal from "gi://Astal?version=3.0"
+import Gtk from "gi://Gtk?version=3.0";
 
 type StatusIconProps = {
     icon_name: Binding<string> | string;
     tooltip?: Binding<string> | string;
     className?: Binding<string> | string;
-    on_click?: any;
-    on_scroll?: any;
+    on_click?: ((self: Gtk.Widget, ev: Astal.ClickEvent) => unknown) | string | string[];
+    on_scroll?: ((self: Gtk.Widget, ev: Astal.ScrollEvent) => unknown) | string | string[];
 };
 
 export default function StatusIcon(props: StatusIconProps) {
