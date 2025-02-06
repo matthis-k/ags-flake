8ceaf77c36a4353486f832cedb1f1cbef886b1ef
matthis-k <matthis.kaelble@gmail.com>
2025-02-06 06:22:50 +0100

    laucher changes

--- widget/appLauncher.tsx
+++ widget/appLauncher.tsx
@@ -72,7 +72,7 @@ export default function Applauncher() {
 
     return <window
         name="launcher"
-        className="Launcher"
+        className="hyprshell-launcher"
         anchor={Astal.WindowAnchor.TOP}
         exclusivity={Astal.Exclusivity.IGNORE}
         keymode={Astal.Keymode.EXCLUSIVE}
